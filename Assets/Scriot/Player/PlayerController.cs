7e49de54ae98f291cd56ac8584abcc01f79bca70
oMJ25o <menardaustero@gmail.com>
2023-04-22 06:13:08 +0800

    Player Roll Movement and Boundaries Done
    - added functionality to let player roll with cooldown
    - added boundaries in the left and right border

--- Assets/Scriot/Player/PlayerController.cs
+++ Assets/Scriot/Player/PlayerController.cs
@@ -5,24 +5,73 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float rollSpeed = 2f;
+    [SerializeField] private float rollLength = 0.5f;
+    [SerializeField] private float rollCoolDown = 1f;
+    [SerializeField] private float leftBorder;
+    [SerializeField] private float rightBorder;
+
+    private float newPlayerSpeed;
+    private float rollCounter;
+    private float rollCoolCounter;
+    private float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        newPlayerSpeed = playerSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        MovePlayer(horizontalInput);
+        MovePlayer();
+        RollPlayer();
     }
 
-    private void MovePlayer(float horizontalInput)
+    private void MovePlayer()
     {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
         GetComponent<Animator>().SetFloat("Horizontal", horizontalInput);
-        gameObject.transform.Translate(transform.right * playerSpeed * Time.deltaTime * horizontalInput);
+        gameObject.transform.Translate(transform.right * newPlayerSpeed * Time.deltaTime * horizontalInput);
+        if (gameObject.transform.position.x < -leftBorder)
+        {
+            gameObject.transform.position = new Vector3(-leftBorder, transform.position.y, transform.position.z);
+        }
+        else if (gameObject.transform.position.x > rightBorder)
+        {
+            gameObject.transform.position = new Vector3(rightBorder, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void RollPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (horizontalInput > 0 || horizontalInput < 0))
+        {
+            if (rollCoolCounter <= 0 && rollCounter <= 0)
+            {
+                newPlayerSpeed = playerSpeed * rollSpeed;
+                rollCounter = rollLength;
+                GetComponent<Animator>().SetBool("SpaceBool", true);
+            }
+        }
+
+        if (rollCounter > 0)
+        {
+            rollCounter -= Time.deltaTime;
+        }
+
+        if (rollCoolCounter > 0)
+        {
+            rollCoolCounter -= Time.deltaTime;
+        }
+    }
+
+    private void RollPlayerFinish()
+    {
+        GetComponent<Animator>().SetBool("SpaceBool", false);
+        newPlayerSpeed = playerSpeed;
+        rollCoolCounter = rollCoolDown;
     }
 
 }
