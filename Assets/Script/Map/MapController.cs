e140b8e5c322977faf2b0d2a3148787fbcd79d65
oMJ25o <menardaustero@gmail.com>
2023-08-19 15:11:40 +0800

    Added Tutorial Scene

--- Assets/Script/Map/MapController.cs
+++ Assets/Script/Map/MapController.cs
@@ -7,12 +7,27 @@ public class MapController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MapSelection(int cave)
+    {
+        switch (cave)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
     }
 }
